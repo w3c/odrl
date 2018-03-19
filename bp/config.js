48f91351767ef35a10795186a7d69528d51012e5
Renato Iannella <r@iannel.la>
2018-03-19 23:11:46 +1000

    added best-practices

---
+++ bp/config.js
@@ -0,0 +1,36 @@
+var respecConfig = {
+    specStatus: "ED",
+    shortName: "odrl-bp",
+    // publishDate:          "2016-10-18",
+    edDraftURI: "https://w3c.github.io/poe/bp/",
+    editors: [	{   name:       "Benedict Whittam Smith",
+         	company:    "Thomson Reuters"},
+         	 {  name:       "Víctor Rodríguez-Doncel",
+                    url:        "https://www.linkedin.com/in/victor-rodriguez-doncel-554b5213/en",
+                    company:    "Universidad Politécnica de Madrid",
+                   companyURL: "http://www.upm.es",
+                    mailto:     "vrodriguez@fi.upm.es"
+           }
+    ],
+    wg: "Permissions &amp; Obligations Expression Working Group",
+    wgURI: "https://www.w3.org/2016/poe/",
+    edDraftURI:   "https://w3c.github.io/poe/bp/",
+    wgPublicList: "public-poe-comments",
+    wgPatentURI: "http://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    lint: true,
+    issueBase: "https://www.w3.org/2016/poe/track/issues/",
+    otherLinks: [
+        {
+            key: "Issue list",
+            data: [ {
+                value: "Github Repository",
+                href: "https://github.com/w3c/poe/issues?q=is%3Aopen+is%3Aissue+label%3A%22Best+Practices%22"
+            }
+            ]
+        }
+    ]
+};
