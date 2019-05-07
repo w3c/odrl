1a2d7f2ea33ae6955455a121f57b8f116b739762
Renato Iannella <r@iannel.la>
2019-05-07 23:22:08 +1000

    added Profile BP

---
+++ profile-bp/config.js
@@ -0,0 +1,35 @@
+var respecConfig = {
+    specStatus: "CG-DRAFT",
+    shortName: "profile-bp",
+    // publishDate:          "2016-10-18",
+    edDraftURI: "https://w3c.github.io/poe/bp/",
+    editors: [
+         	 {  name:       "Michael Steidl,",
+                    url:        "https://uk.linkedin.com/in/michaelwsteidl",
+                    company:    "NewsIT",
+                   companyURL: "http://newsit.biz",
+                    mailto:     "mwsteidl@newsit.biz"
+           }
+    ],
+    wg: "ODRL Community Group",
+    wgURI: "https://www.w3.org/community/odrl/",
+    edDraftURI:   "https://w3c.github.io/odrl/profile-bp/",
+    wgPublicList: "https://lists.w3.org/Archives/Public/public-odrl/",
+    //wgPatentURI: "http://www.w3.org/2004/01/pp-impl/87755/status",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    lint: true,
+    issueBase: "https://github.com/w3c/odrl/issues",
+    otherLinks: [
+        {
+            key: "Issue list",
+            data: [ {
+                value: "Github Repository",
+                href: "https://github.com/w3c/odrl/issues"
+            }
+            ]
+        }
+    ]
+};
