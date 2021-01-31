3e438f8dcd831eba8383a4215ac72d3b95ec7278
Renato Iannella <r@iannel.la>
2021-02-01 09:41:55 +1000

    ODRL formal semantics WD

---
+++ formal-semantics/config.js
@@ -0,0 +1,36 @@
+var respecConfig = {
+    specStatus: "CG-DRAFT",
+    shortName: "formal-semantics",
+    edDraftURI: "https://w3c.github.io/odrl/profile-temporal/",
+    editors: [	{   name:       "Name",
+                     url:        "linkedIn URL",
+                     company:    "Org",
+                     companyURL: "Org URL",
+                      mailto:     "person email"},
+         	 {  name:       "Name",
+                    url:        "linkedIn URL",
+                    company:    "Org",
+                   companyURL: "Org URL",
+                    mailto:     "person email"
+           }
+    ],
+    wg: "ODRL Community Group",
+    wgURI: "https://www.w3.org/community/odrl/",
+    wgPublicList: "https://lists.w3.org/Archives/Public/public-odrl/",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    lint: true,
+    issueBase: "https://github.com/w3c/odrl/issues",
+    otherLinks: [
+        {
+            key: "Issue list",
+            data: [ {
+                value: "Github Repository",
+                href: "https://github.com/w3c/odrl/issues"
+            }
+            ]
+        }
+    ]
+};
