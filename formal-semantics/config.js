4ee821d5438846fa99bbfa94aeaee02499975f9a
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-10-27 15:31:12 +0100

    add access control scenario to formal-semantics document

--- formal-semantics/config.js
+++ formal-semantics/config.js
@@ -1,18 +1,30 @@
 var respecConfig = {
     specStatus: "CG-DRAFT",
     shortName: "formal-semantics",
-    editors: [	{   name:       "Nicoletta Fornara",
-                     url:        "https://www.linkedin.com/in/nicoletta-fornara-0384397",
-                     company:    "Universita' della Svizzera italiana",
-                     companyURL: "https://www.usi.ch/en",
-                      mailto:     "person email"},
-         	 {  name:       "Víctor Rodríguez-Doncel",
-                    url:        "http://cosasbuenas.es",
-                    company:    "Ontology Engineering Group, Universidad Politécnica de Madrid",
-                   companyURL: "http://oeg-upm.net",
-                    mailto:     "n.a."
-           }
-    ],
+    editors: 
+        [
+            {
+                name: "Nicoletta Fornara",
+                url: "https://www.linkedin.com/in/nicoletta-fornara-0384397",
+                company: "Universita' della Svizzera italiana",
+                companyURL: "https://www.usi.ch/en",
+                mailto: "person email"    
+            },
+         	{  
+                name: "Víctor Rodríguez-Doncel",
+                url: "http://cosasbuenas.es",
+                company: "Ontology Engineering Group, Universidad Politécnica de Madrid",
+                companyURL: "https://oeg.fi.upm.es/",
+                mailto: "n.a."
+            },
+            {  
+                name: "Beatriz Esteves",
+                url: "https://besteves4.github.io",
+                company: "Ontology Engineering Group, Universidad Politécnica de Madrid",
+                companyURL: "https://oeg.fi.upm.es/",
+                mailto: "beatriz.gesteves@upm.es"
+            },
+        ],
     group: "odrl",
     wgPublicList: "public-odrl",
     inlineCSS: true,
