48fbf6cbeef2db2926797ce6183ab08f8079c00e
Beatriz Esteves <60222453+besteves4@users.noreply.github.com>
2022-11-08 13:43:48 +0000

    add Connect an odrl:Agreement with an odrl:Offer and an odrl:Request example

--- bp/config.js
+++ bp/config.js
@@ -1,15 +1,27 @@
 var respecConfig = {
     specStatus: "CG-DRAFT",
     shortName: "odrl-bp",
-    editors: [	{   name:       "Benedict Whittam Smith",
-         	company:    "Thomson Reuters"},
-         	 {  name:       "Víctor Rodríguez-Doncel",
-                    url:        "https://www.linkedin.com/in/victor-rodriguez-doncel-554b5213/en",
-                    company:    "Universidad Politécnica de Madrid",
-                   companyURL: "http://www.upm.es",
-                    mailto:     "vrodriguez@fi.upm.es"
-           }
-    ],
+    editors: 
+        [
+            {
+                name: "Benedict Whittam Smith",
+         	    company: "Thomson Reuters"
+            },
+            {
+                name: "Víctor Rodríguez-Doncel",
+                url: "https://www.linkedin.com/in/victor-rodriguez-doncel-554b5213/en",
+                company: "Universidad Politécnica de Madrid",
+                companyURL: "http://www.upm.es",
+                mailto: "vrodriguez@fi.upm.es"
+           },
+           {  
+            name: "Beatriz Esteves",
+            url: "https://besteves4.github.io",
+            company: "Ontology Engineering Group, Universidad Politécnica de Madrid",
+            companyURL: "https://oeg.fi.upm.es/",
+            mailto: "beatriz.gesteves@upm.es"
+            }
+        ],
     group: "odrl",
     wgPublicList: "public-odrl",
     inlineCSS: true,
