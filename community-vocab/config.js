2d91320333f2c6758dca6c8d543673e2771bf53f
Renato Iannella <r@iannel.la>
2024-08-10 12:33:24 +1000

    Added community and dataspaces new drafts

---
+++ community-vocab/config.js
@@ -0,0 +1,24 @@
+var respecConfig = { 
+    specStatus: "CG-DRAFT",
+    shortName: "community-vocab",
+    editors: [	{   name:       "Name",
+                     url:        "linkedIn URL",
+                     company:    "Org",
+                     companyURL: "Org URL",
+                      mailto:     "person email"},
+         	 {  name:       "Name",
+                    url:        "linkedIn URL",
+                    company:    "Org",
+                   companyURL: "Org URL",
+                    mailto:     "person email"
+           }
+    ],
+    group: "odrl",
+    wgPublicList: "public-odrl",
+    inlineCSS: true,
+    noIDLIn: true,
+    noLegacyStyle: false,
+    noRecTrack: true,
+    lint: true,
+    github: { repoURL: "https://github.com/w3c/odrl/", branch: "master"}
+};
